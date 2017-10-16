b5e4f1e9cd8a79296444c515d10617335c82eefa
benkristofic <ben.kristofic@gmail.com>
2017-10-16 15:37:04 -0400

    20

--- BasicGit.java
+++ BasicGit.java
@@ -2,7 +2,7 @@ class BasicGit{
 	public static void main (String[] args){
 		int count = 0;
 
-		for (int x = 0; x <=10; x++){
+		for (int x = 0; x <=20; x++){
 			System.out.println(count);
 			count++;
 		}
